a558c21d4ca40b8102dbbb1e3fc6a9d403b4033f
njmola-ops <gjmola@ucsc.edu>
2026-05-23 16:32:52 -0700

    pop-up prototype - Neco

--- final.js
+++ final.js
@@ -5,3 +5,19 @@ startButton.onclick = function () {
     genreSection.style.display = "block";
     startButton.style.display = "none";
 }
+
+let genreSelect = document.getElementById("genres");
+let popup = document.getElementById("popupBox");
+let popupText = document.getElementById("popupText");
+
+genreSelect.addEventListener("change", function () {
+    let value = genreSelect.value;
+
+    popupText.textContent = "You picked: " + value;
+
+    popup.style.display = "block";
+});
+
+popup.onclick = function () {
+    popup.style.display = "none";
+};
