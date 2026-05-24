a41a191c13b4cb1b509e298581d67d4921b5475d
njmola-ops <gjmola@ucsc.edu>
2026-05-23 18:26:47 -0700

    better popup prototype? - Neco

--- final.js
+++ final.js
@@ -9,11 +9,14 @@ startButton.onclick = function () {
 let genreSelect = document.getElementById("genres");
 let popup = document.getElementById("popupBox");
 let popupText = document.getElementById("popupText");
+let popupImage = document.getElementById("popupImage");
 
 genreSelect.addEventListener("change", function () {
     let value = genreSelect.value;
+    let data = genreData[value];
 
-    popupText.textContent = "You picked: " + value;
+    popupText.textContent = data.text;
+    popupImage.src = data.image;
 
     popup.style.display = "block";
 });
@@ -27,3 +30,15 @@ let closeBtn = document.getElementById("closePopup");
 closeBtn.onclick = function () {
     popup.style.display = "none";
 };
+
+let genreData = {
+    action: {
+        text: "'A stuntman, fresh off an almost career-ending accident, has to track down a missing movie star, solve a conspiracy and try to win back the love of his life while still doing his day job.' - IMDb",
+        image: "https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/img_6616.jpeg"
+    }
+};
+
+let value = genreSelect.value;
+
+popupText.textContent = genreData[value].text;
+popupImage.src = genreData[value].image;
